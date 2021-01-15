ea1c61355b322fb203fc510844b6bd90ef3b158d
Andrii Baidachenko <andrewbayd@gmail.com>
2021-01-15 18:24:23 +0100

    Add user model

---
+++ src/main/java/com/conduit/testdata/TestUser.java
@@ -0,0 +1,12 @@
+package com.conduit.testdata;
+
+import com.conduit.models.User;
+
+public class TestUser {
+
+    public static final User TEST_USER_1 = new User("java1@selenium.test", "JavaSeleniumTest1");
+    public static final User TEST_USER_2 = new User("java2@selenium.test", "JavaSeleniumTest2");
+    public static final User TEST_USER_3 = new User("java3@selenium.test", "JavaSeleniumTest3");
+    public static final User TEST_USER_4 = new User("java4@selenium.test", "JavaSeleniumTest4");
+
+}
--- src/test/java/com/conduit/LoginTest.java
+++ src/test/java/com/conduit/LoginTest.java
@@ -1,9 +1,11 @@
 package com.conduit;
 
+import com.conduit.models.User;
 import com.conduit.pages.HomePage;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import static com.conduit.testdata.TestUser.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
@@ -12,20 +14,20 @@ public class LoginTest extends BaseTest {
     @DataProvider(name = "user-credentials", parallel = true)
     public Object[][] returnUserCreds() {
         return new Object[][] {
-                {"java1@selenium.test", "JavaSeleniumTest1"},
-                {"java2@selenium.test", "JavaSeleniumTest2"},
-                {"java3@selenium.test", "JavaSeleniumTest3"},
-                {"java4@selenium.test", "JavaSeleniumTest4"}
+                {TEST_USER_1},
+                {TEST_USER_2},
+                {TEST_USER_3},
+                {TEST_USER_4}
         };
     }
 
     @Test(dataProvider = "user-credentials")
-    void loginTest(String email, String password) {
+    void loginTest(User user) {
         HomePage homePage = new HomePage();
         homePage
                 .open()
                 .navigateToLoginPage()
-                .loginAs(email, password);
+                .loginAs(user.getEmail(), user.getPassword());
         assertThat(homePage.isNewArticleButtonDisplayed(), equalTo(true));
     }
 }
