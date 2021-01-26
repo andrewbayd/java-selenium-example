c131483b6e72e7d288632b4256652b33a2aecbac
Andrii Baidachenko <andrewbayd@gmail.com>
2021-01-26 17:57:33 +0100

    Fix auth client

--- src/main/java/com/conduit/api/clients/AuthClient.java
+++ src/main/java/com/conduit/api/clients/AuthClient.java
@@ -1,8 +1,9 @@
 package com.conduit.api.clients;
 
 import com.conduit.models.User;
-import com.google.gson.JsonObject;
 
+import java.util.HashMap;
+import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 import static io.restassured.http.ContentType.JSON;
@@ -10,16 +11,9 @@ import static io.restassured.http.ContentType.JSON;
 public class AuthClient {
 
     public String getToken(String email,  String password) {
-        JsonObject userCreds = new JsonObject();
-        userCreds.addProperty("email", email);
-        userCreds.addProperty("password", password);
-
-        JsonObject userObject = new JsonObject();
-        userObject.add("user", userCreds);
-
         return given().
                 contentType(JSON).
-                body(userObject).
+                body(getUserJson(email, password)).
         when().
                 post("https://conduit.productionready.io/api/users/login").
         then().
@@ -31,5 +25,14 @@ public class AuthClient {
 
     public String getToken(User user) {
         return getToken(user.getEmail(), user.getPassword());
+    }
+
+    private Map<String, Object> getUserJson(String email, String password) {
+        Map<String, String> userCreds = new HashMap<>();
+        userCreds.put("email", email);
+        userCreds.put("password", password);
+        Map<String, Object> userJson = new HashMap<>();
+        userJson.put("user", userCreds);
+        return userJson;
     }
 }
--- src/main/java/com/conduit/utils/AuthHelper.java
+++ src/main/java/com/conduit/utils/AuthHelper.java
@@ -2,9 +2,13 @@ package com.conduit.utils;
 
 import com.conduit.api.clients.AuthClient;
 import com.conduit.models.User;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import static lombok.AccessLevel.PRIVATE;
+
 @Slf4j
+@NoArgsConstructor(access = PRIVATE)
 public class AuthHelper {
 
     public static String getAuthToken(String email, String password) {
